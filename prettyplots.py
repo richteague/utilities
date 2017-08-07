e2afeff5da612a871c566b038796a99403d38297
richteague <richarddteague@gmail.com>
2017-08-07 15:57:05 +0200

    allow for asymmetric uncertainties, percentiles and outlined median values

--- prettyplots.py
+++ prettyplots.py
@@ -9,38 +9,90 @@ def gaussian(x, dx, A=1.0, x0=0.0, offset=0.0):
 
 def gradient_between(x, y, dy, ax=None, **kwargs):
     """Similar to fill_between but with a gradient."""
+
     if ax is None:
         fig, ax = plt.subplots()
 
-    # Gradiated fill.
-    fc = kwargs.get('gradcolor', 'dodgerblue')
+    # If dy has a shape (x.size, 2) or (2, x.size) then we assume that these
+    # are percentiles and y is the median. If so, we can convert these to
+    # uncertainties for the plotting. Otherwise we just double up the
+    # uncertainties.
+
+    try:
+        ndim = dy.ndim
+    except AttributeError:
+        dy = np.squeeze(dy)
+        ndim = dy.ndim
+
+    if ndim == 1:
+        dy = np.array([dy, dy])
+    elif ndim == 2:
+        if dy.shape[1] == 2:
+            dy = dy.T
+        if kwargs.get('percentiles', False):
+            dy = np.squeeze([y - dy[0], dy[1] - y])
+    else:
+        raise TypeError("'dy' must be only 1D or 2D.")
+    if not (dy.shape[0] == 2 and dy.shape[1] == x.size):
+        raise ValueError()
+
+    # Populate the colours and styles of the plotting.
+    # Colors, can set each individually but 'color' will override all.
+
+    color = kwargs.pop('color', None)
+    if color is not None:
+        fc = color
+        lc = color
+        ec = [color]
+    else:
+        fc = kwargs.get('gradcolor', 'dodgerblue')
+        lc = kwargs.get('linecolor', 'dodgerblue')
+        ec = np.array([kwargs.get('edgecolor', 'k')]).flatten()
+
+    # Controlls the gradient fill. The gradient will run from an alpha of
+    # alphamax at the median to ~0 at y +/- nsigma * dy. This will be built
+    # from nfill fill_between calls.
+
     am = kwargs.get('alphamax', .7)
     ns = kwargs.get('nsigma', 3)
     fn = kwargs.get('nfills', 15)
-    fy = np.linspace(0., ns, fn)
 
-    # Calculate the appropriate alpha for the layer.
-    alphacum = 0.0
-    for n in fy[::-1]:
-        alpha = np.mean(gaussian(n, ns / 3., am)) - alphacum
-        ax.fill_between(x, y-n*dy, y+n*dy, facecolor=fc, lw=0, alpha=alpha)
-        alphacum += alpha
+    # Styles for the percentiles. Will cycle through the values if given in a
+    # list. Should not complain if the lists are different lenghts.
 
-    # Properties for the edges. These are able to be interated over.
+    lw = kwargs.get('linewidth', 1.25)
+    ms = kwargs.get('markersize', 3)
     ed = np.array([kwargs.get('edges', [1, 3])]).flatten()
     ea = np.array([kwargs.get('edgealpha', [0.5, 0.25])]).flatten()
     es = np.array([kwargs.get('edgestyle', ':')]).flatten()
-    ec = np.array([kwargs.get('edgecolor', 'k')]).flatten()
+
+    # Incrementally calculate the alpha for a given layer and plot it.
+
+    alphacum = 0.0
+    fy = np.linspace(0., ns, fn)
+    for n in fy[::-1]:
+        alpha = np.mean(gaussian(n, ns / 3., am)) - alphacum
+        ax.fill_between(x, y-n*dy[0], y+n*dy[1], facecolor=fc, lw=0,
+                        alpha=alpha)
+        alphacum += alpha
+
+    # Properties for the edges. These are able to be interated over.
+
     for e, edge in enumerate(ed):
-        ax.plot(x, y-edge*dy, alpha=ea[e % len(ea)], color=ec[e % len(ec)],
+        ax.plot(x, y-edge*dy[0], alpha=ea[e % len(ea)], color=ec[e % len(ec)],
                 linestyle=es[e % len(es)])
-        ax.plot(x, y+edge*dy, alpha=ea[e % len(ea)], color=ec[e % len(ec)],
+        ax.plot(x, y+edge*dy[1], alpha=ea[e % len(ea)], color=ec[e % len(ec)],
                 linestyle=es[e % len(es)])
 
     # Mean value including the label. Note that we do not call the legend here
-    # so extra legend kwargs can be used if necessary.
-    lc = kwargs.get('linecolor', 'k')
-    ax.plot(x, y, color=lc, label=kwargs.get('label', None))
+    # so extra legend kwargs can be used if necessary. If 'outline' is true,
+    # include a slightly thicker line in black.
+
+    if kwargs.get('outline', True):
+        ax.errorbar(x, y, color='k', fmt=kwargs.get('fmt', '-o'),
+                    ms=ms, mew=1, lw=lw*2)
+    ax.errorbar(x, y, color=lc, fmt=kwargs.get('fmt', '-o'),
+                ms=ms, mew=0, lw=lw, label=kwargs.get('label', None))
 
     return ax
 
